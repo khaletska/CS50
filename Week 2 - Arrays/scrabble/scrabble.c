40274d173305f96ee6804233c06e2c36005c54b7
khaletska <khaletska.e@gmail.com>
2024-05-19 20:55:19 +0300

    Solve scrabble v.2

--- Week 2 - Arrays/scrabble/scrabble.c
+++ Week 2 - Arrays/scrabble/scrabble.c
@@ -1,23 +1,20 @@
 #include <cs50.h>
 #include <ctype.h>
 #include <stdio.h>
+#include <string.h>
 
-int getIndex(char key);
-int getValue(char key);
 int countPoints(string word);
 
-int SIZE = 26;
-char KEYS[26] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
-                 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
-int VALUES[26] = {1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3, 1, 1, 3, 10, 1, 1, 1, 1, 4, 4, 8, 4, 10};
+const int VALUES[] = {1, 3, 3, 2,  1, 4, 2, 4, 1, 8, 5, 1, 3,
+                      1, 1, 3, 10, 1, 1, 1, 1, 4, 4, 8, 4, 10};
 
 int main(void)
 {
-    string p1 = get_string("Player1: ");
-    string p2 = get_string("Player2: ");
+    string player1 = get_string("Player1: ");
+    string player2 = get_string("Player2: ");
 
-    int score1 = countPoints(p1);
-    int score2 = countPoints(p2);
+    int score1 = countPoints(player1);
+    int score2 = countPoints(player2);
 
     if (score1 > score2)
     {
@@ -33,40 +30,21 @@ int main(void)
     }
 }
 
-int getIndex(char key)
-{
-    char uppercase = toupper(key);
-    for (int i = 0; i < SIZE; i++)
-    {
-        if (KEYS[i] == uppercase)
-        {
-            return i;
-        }
-    }
-    return -1;
-}
-
-int getValue(char key)
-{
-    int index = getIndex(key);
-    if (index == -1)
-    {
-        return 0;
-    }
-    else
-    {
-        return VALUES[index];
-    }
-}
 
 int countPoints(string word)
 {
-    int n = 0;
+    int wordLength = strlen(word);
     int points = 0;
-    while (word[n] != 0)
+    for (int i = 0; i < wordLength; i++)
     {
-        points += getValue(word[n]);
-        n++;
+        if (!isalpha(word[i]))
+        {
+            continue;
+        }
+        else
+        {
+            points += VALUES[toupper(word[i]) - 65];
+        }
     }
     return points;
 }
