83e84ff6432d49368766e9fa2957d0e79272a263
Elena Khaletska <khaletska.e@gmail.com>
2024-05-22 18:55:07 +0300

    Problem Set 4 - solve volume (#7)

--- Week_4_Memory/volume/volume.c
+++ Week_4_Memory/volume/volume.c
@@ -33,9 +33,18 @@ int main(int argc, char *argv[])
 
     float factor = atof(argv[3]);
 
-    // TODO: Copy header from input file to output file
+    // Copy header from input file to output file
+    uint8_t header[HEADER_SIZE];
+    fread(&header, HEADER_SIZE, 1, input);
+    fwrite(&header, HEADER_SIZE, 1, output);
 
-    // TODO: Read samples from input file and write updated data to output file
+    // Read samples from input file and write updated data to output file
+    int16_t b;
+    while (fread(&b, sizeof(b), 1, input) != 0)
+    {
+        b = b * factor;
+        fwrite(&b, sizeof(b), 1, output);
+    }
 
     // Close files
     fclose(input);
